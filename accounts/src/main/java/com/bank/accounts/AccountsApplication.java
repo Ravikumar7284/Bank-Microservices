ab3fa8be4fa714ec2411431a685d9a4dacb7adfa
Ravikumar7284 <ravikumarrajhans11@outlook.com>
2025-04-12 20:38:07 +0530

    yml and xml files added

---
+++ accounts/src/main/java/com/bank/accounts/AccountsApplication.java
@@ -0,0 +1,24 @@
+package com.bank.accounts;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Account Microservice",
+				description = "Account Microservice API Documentation",
+				version = "v1"
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
