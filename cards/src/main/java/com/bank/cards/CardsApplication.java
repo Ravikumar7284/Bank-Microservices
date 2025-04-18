280bb53ff651add06e849eeb9beed08870230550
Ravikumar7284 <ravikumarrajhans11@outlook.com>
2025-04-18 18:28:30 +0530

    Cards- extra changes

--- cards/src/main/java/com/bank/cards/CardsApplication.java
+++ cards/src/main/java/com/bank/cards/CardsApplication.java
@@ -4,8 +4,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@ComponentScan("com.bank.cards")
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @SpringBootApplication
 @OpenAPIDefinition(
