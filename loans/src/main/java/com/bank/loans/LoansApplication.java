9358a8dc363d83c1f50bb2731e18020526729f2a
Ravikumar7284 <ravikumarrajhans11@outlook.com>
2025-09-17 22:46:42 +0530

    loans - configuration using @configurationproperties

--- loans/src/main/java/com/bank/loans/LoansApplication.java
+++ loans/src/main/java/com/bank/loans/LoansApplication.java
@@ -1,15 +1,18 @@
 package com.bank.loans;
 
+import com.bank.loans.dto.LoansContactInfoDto;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @ComponentScan("com.bank.loans")
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = LoansContactInfoDto.class)
 @OpenAPIDefinition(
     info = @Info(
         title = "Loans Microservice",
