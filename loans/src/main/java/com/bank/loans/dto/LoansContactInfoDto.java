293263bb79bb735cf82b0473f8eaf5e438c9f3cc
Ravikumar7284 <ravikumarrajhans11@outlook.com>
2025-12-23 13:22:43 +0530

    runtime config properties refresh using actuator path

--- loans/src/main/java/com/bank/loans/dto/LoansContactInfoDto.java
+++ loans/src/main/java/com/bank/loans/dto/LoansContactInfoDto.java
@@ -2,9 +2,13 @@ package com.bank.loans.dto;
 
 import java.util.List;
 import java.util.Map;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "loans-contact-info")
-public record LoansContactInfoDto(String message, Map<String, String> contactDetails,
-                                  List<String> contactSupport) {
+@Data
+public class LoansContactInfoDto {
+  private String message;
+  private Map<String, String> contactDetails;
+  private List<String> contactSupport;
 }
